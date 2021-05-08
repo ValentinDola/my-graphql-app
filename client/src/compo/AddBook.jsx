9dae4a17aad759f1258cae9b9a0c0a3de814ee3d
Dola Valentino <dolavalentino@gmail.com>
2021-05-08 12:18:07 +0000

    Format documents

--- client/src/compo/AddBook.jsx
+++ client/src/compo/AddBook.jsx
@@ -1,72 +1,57 @@
-import React from 'react';
+import React from "react";
 
 //Apollo client imports
-import {useQuery, gql} from '@apollo/client';
+import { useQuery, gql } from "@apollo/client";
 
-
-const getAllAuthors = gql `
-    {
-        authors {
-            name
-            age
-            id
-        }
+const getAllAuthors = gql`
+  {
+    authors {
+      name
+      age
+      id
     }
+  }
 `;
 
-
-
 const AddBook = () => {
+  const { data, error, loading } = useQuery(getAllAuthors);
 
-    const {data, error, loading} = useQuery(getAllAuthors);
+  // if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+  if (data) {
+    console.log(data);
+  }
 
-    // if (loading) return <p>Loading...</p>;
-    if (error) return <p>Error :(</p>;
-    if (data) {
-        console.log(data)
-    }
+  const selectAuthor = () => {
+    return loading ? (
+      <option>Loading Authors </option>
+    ) : (
+      data.authors.map((author) => <option>{author.name}</option>)
+    );
+  };
 
-    const selectAuthor = () => {
-
-        return (
-
-            loading ? <option>Loading Authors </option> :
-                data.authors.map(author => (
-                    <option>{author.name}</option>
-                ))
-
-
-        )
-
-    }
-
-    return (
-        <div>
-           <form id={'add_book'}>
-
-               <div className={'form_field'} >
-                   <label>Book name</label>
-                   <input type={'text'} />
-               </div>
-
-               <div className={'form_field'} >
-                   <label>Genre</label>
-                   <input type={'text'} />
-               </div>
-
-               <div className={'form_field'} >
-                   <label>Author</label>
-                   <select>
-                       {selectAuthor()}
-                   </select>
-               </div>
-
-               <button>+</button>
-
-           </form>
+  return (
+    <div>
+      <form id={"add_book"}>
+        <div className={"form_field"}>
+          <label>Book name</label>
+          <input type={"text"} />
         </div>
-    )
 
-}
+        <div className={"form_field"}>
+          <label>Genre</label>
+          <input type={"text"} />
+        </div>
+
+        <div className={"form_field"}>
+          <label>Author</label>
+          <select>{selectAuthor()}</select>
+        </div>
+
+        <button>+</button>
+      </form>
+    </div>
+  );
+};
 
 export default AddBook;
--- client/src/compo/BookList.jsx
+++ client/src/compo/BookList.jsx
@@ -1,46 +1,40 @@
-import React from 'react';
+import React from "react";
 
 //Apollo client imports
-import {useQuery, gql} from '@apollo/client';
-
+import { useQuery, gql } from "@apollo/client";
 
 //Making queries
 const getAllTheBooks = gql`
-    {
-        books {
-            name
-            genre
-        }
+  {
+    books {
+      name
+      genre
     }
+  }
 `;
 
 const BookList = () => {
+  const { data, error, loading } = useQuery(getAllTheBooks);
 
-    const {data, error, loading} = useQuery(getAllTheBooks);
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+  if (data) {
+    console.log(data);
+  }
 
-    if (loading) return <p>Loading...</p>;
-    if (error) return <p>Error :(</p>;
-    if (data) {
-        console.log(data)
-    }
+  const bookList = () => {
+    return data.books.map((book) => (
+      <li>
+        the book name {book.name} and the genre {book.genre}
+      </li>
+    ));
+  };
 
-    const bookList = () => {
-        return (
-            data.books.map(book => (
-                    <li>
-                        the book name {book.name} and the genre {book.genre}
-                    </li>
-                ) )
-        )
-    }
-
-    return (
-        <div>
-            <ul>
-                {bookList()}
-            </ul>
-        </div>
-    )
-}
+  return (
+    <div>
+      <ul>{bookList()}</ul>
+    </div>
+  );
+};
 
 export default BookList;
